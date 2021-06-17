723800facaffce2d89ed146d0f4e3305717c36db
asifistiaque <asifistiaque.ai@gmail.com>
2021-06-17 23:59:08 +0600

    asd

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import '../styles/style.scss';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	const handleRouteChange = url => {
+		window.gtag('config', 'G-9SJ0WWVELQ', {
+			page_path: url,
+		});
+	};
+
+	useEffect(() => {
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<Provider store={store}>
 			<Component {...pageProps} />
