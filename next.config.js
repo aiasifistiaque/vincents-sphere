2528accc3046fb0e645f0d82b9e2165b5c0ecef2
asifistiaque <asifistiaque.ai@gmail.com>
2021-07-04 01:55:31 +0600

    asds

--- next.config.js
+++ next.config.js
@@ -1,10 +1,44 @@
 // next.config.js
 
+const securityHeaders = [
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block',
+	},
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=31536000; includeSubDomains; preload',
+	},
+
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin',
+	},
+];
+
 module.exports = {
 	images: {
 		domains: ['vincent-backend.herokuapp.com'],
 	},
 	image: {
 		domains: ['vincent-backend.herokuapp.com'],
+	},
+
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/(.*)',
+				headers: securityHeaders,
+			},
+		];
 	},
 };
